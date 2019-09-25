773b57188d7969c2bee3c10f9d34fa79b15b6269
ziyang <zishuo.yang@cern.ch>
2019-09-25 23:35:57 +0200

    Add ganga script for merging root files with URLs

--- tupleProduction/mergeRootURLs.py
+++ tupleProduction/mergeRootURLs.py
@@ -1,57 +1,69 @@
-## For merging into multiple root files:
+## Ganga script
+## for merging (multiple) root files from grid jobs' URLs
+## Zishuo Yang, 2019-09-25
+
 import ROOT
 import sys
 
-targetDir = "/afs/cern.ch/work/z/ziyang/Data2016_stripped"
-targetFileName = "2016_MU_ntuple_"
+targetDir = "/eos/user/z/ziyang/RJpsi/tuples/"
+targetFileName = "2012_MC_JpsiMuNu"
+sourceTreeName = "JpsiRecTuple/DecayTree"
 
 fname = str(sys.argv[1])
 with open(fname) as f:
     content = f.readlines()
+print("length of sys.argv = "+str(len(sys.argv)))
+if len(sys.argv) == 3: # For combining MagUp and MagDown
+    fname2 = str(sys.argv[2])
+    with open(fname2) as f2:
+        content2 = f2.readlines()
+        print("content2: ", len(content2))
+    content = content + content2
+    print("content: ", len(content))
+
 # Divide up the input URL files 
-numInputFiles = 16
+numInputFiles = 100
 numLast = len(content)%numInputFiles
 if numLast == 0:
     numOutputFiles = int(len(content)/numInputFiles)
 else:
     numOutputFiles = 1 + int(len(content)/numInputFiles)
-print("-> To merge into "+str(numOutputFiles)+" output files...")
+print("-> To merge "+str(len(content))+" input files into "+str(numOutputFiles)+" output files...")
 listFileNames = []
-for i in range(0,numOutputFiles):
-    fullName = targetDir+targetFileName+str(i)+".root"
-    listFileNames.append(fullName)
 
-for i in range(0,numOutputFiles):
-#    if i == (numOutputFiles-1):#for the last output file case
-#        if numLast == 0:
-#            continue
-#        else:
-#            print("Start merging "+listFileNames[i])
-#            startIndex = i*numInputFiles
-#            endIndex = i*numInputFiles + numLast
-#            chain = ROOT.TChain("BuTuple/DecayTree")
-#            for url in content[startIndex:endIndex]: #first 20 files
-#                url = url.rstrip('\n')
-#                print("adding file: "+url)
-#                chain.AddFile(url)
-#            chain.Merge(listFileNames[i])
-#            print("Done merging "+listFileNames[i])
-#            break
-            
-    print("-> Start merging "+listFileNames[i])
-    startIndex = i*numInputFiles
-    endIndex = (i+1)*numInputFiles
-    if i==(numOutputFiles-1) and numLast!=0:
-        endIndex = i*numInputFiles + numLast
-    chain = ROOT.TChain("BuTuple/DecayTree")
-    for url in content[startIndex:endIndex]: #first 20 files
+if numOutputFiles == 1:
+    fullName = targetDir+targetFileName+".root"
+    listFileNames.append(fullName)
+    print("-> Start merging "+listFileNames[0])
+    chain = ROOT.TChain(sourceTreeName)
+    for url in content:
         url = url.rstrip('\n')
         print("adding file: "+url)
         chain.AddFile(url)
-    chain.Merge(listFileNames[i])
-    print("-> Done merging "+listFileNames[i])
-
- 
-#chain.Draw("Bu_M") 
-#raw_input("press ENTER to close")
-
+    chain.Merge(listFileNames[0])
+    print("-> Done merging "+listFileNames[0])
+    chain.Draw("Bc_M") 
+    raw_input("press ENTER to close")
+    
+elif numOutputFiles > 1:
+    for i in range(0,numOutputFiles):
+        fullName = targetDir+targetFileName+str(i)+".root"
+        listFileNames.append(fullName)
+    
+    for i in range(0,numOutputFiles):
+        print("-> Start merging "+listFileNames[i])
+        startIndex = i*numInputFiles
+        endIndex = (i+1)*numInputFiles
+        if i==(numOutputFiles-1) and numLast!=0:
+            endIndex = i*numInputFiles + numLast
+        chain = ROOT.TChain("BuTuple/DecayTree")
+        for url in content[startIndex:endIndex]: #first 20 files
+            url = url.rstrip('\n')
+            print("adding file: "+url)
+            chain.AddFile(url)
+        chain.Merge(listFileNames[i])
+        print("-> Done merging "+listFileNames[i])
+        #chain.Draw("Bu_M") 
+        #raw_input("press ENTER to close")
+else:
+    print("Error: number of output files = "+str(numOutputFiles))
